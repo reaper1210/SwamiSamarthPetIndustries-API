ae2ab29e7436e14382769fd9f29e1638f0e46a62
Vinesh05 <lundvinesh@gmail.com>
2021-10-21 18:23:20 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.swamisamarthpet.sspi-api"
---
+++ src/main/kotlin/com/swamisamarthpet/Application.kt
@@ -0,0 +1,14 @@
+package com.swamisamarthpet
+
+import io.ktor.application.*
+import com.swamisamarthpet.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+    configureMonitoring()
+}

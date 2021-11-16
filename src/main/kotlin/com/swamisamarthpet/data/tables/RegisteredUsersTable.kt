a07e50dff21321fb3a3c0749eca8ae3ac586ed7e
Soham-03 <sohamparab1003@gmail.com>
2021-11-16 11:39:04 +0530

    Create User and User Messages Table

--- src/main/kotlin/com/swamisamarthpet/data/tables/RegisteredUsersTable.kt
+++ src/main/kotlin/com/swamisamarthpet/data/tables/RegisteredUsersTable.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
 object RegisteredUsersTable:Table() {
-    val userId: Column<String> = varchar("userId",10)
+    val userId: Column<String> = varchar("userId",10485760)
     val userName: Column<String> = varchar("userName",500)
     val phoneNumber: Column<String> = varchar("phoneNumber",13).uniqueIndex()
 

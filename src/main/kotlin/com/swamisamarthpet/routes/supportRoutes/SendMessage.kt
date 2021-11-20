f4ea063957ef49ee2843dc460c20a97b8ae859e5
Soham-03 <sohamparab1003@gmail.com>
2021-11-20 18:13:09 +0530

    Testing WebSocket

--- src/main/kotlin/com/swamisamarthpet/routes/supportRoutes/SendMessage.kt
+++ src/main/kotlin/com/swamisamarthpet/routes/supportRoutes/SendMessage.kt
@@ -14,16 +14,15 @@ fun Route.sendMessage(){
     webSocket("$API_VERSION/sendMessage"){
         val sessionId = generateSessionId()
         val socket = this
-        val parameters = call.receive<Parameters>()
-        val userId = parameters["userId"]?: return@webSocket call.respond(HttpStatusCode.Unauthorized,"Missing User Id")
-        val message = parameters["message"]?: return@webSocket call.respond(HttpStatusCode.Unauthorized,"Missing Message")
-        val dateAndTime = parameters["dateAndTime"]?: return@webSocket call.respond(HttpStatusCode.Unauthorized,"Missing Date And Time")
-        val messageFrom = parameters["messageFrom"]?: return@webSocket call.respond(HttpStatusCode.Unauthorized,"Missing Message From")
+        val userId = call.parameters["userId"]?: return@webSocket call.respond(HttpStatusCode.Unauthorized,"Missing User Id")
+        val message = call.parameters["message"]?: return@webSocket call.respond(HttpStatusCode.Unauthorized,"Missing Message")
+        val dateAndTime = call.parameters["dateAndTime"]?: return@webSocket call.respond(HttpStatusCode.Unauthorized,"Missing Date And Time")
+        val messageFrom = call.parameters["messageFrom"]?: return@webSocket call.respond(HttpStatusCode.Unauthorized,"Missing Message From")
 
         try{
             val member = SupportRepo().onJoin(userId,sessionId,socket)
             val result = SupportRepo().sendMessage(userId, message, dateAndTime, messageFrom,member)
-            call.respond(HttpStatusCode.OK,result)
+//            call.respond(HttpStatusCode.OK,result)
         }
         catch (e:Throwable){
             call.respondText(e.message.toString())
